1298ac1ef1a60e3a32280905ebf6507d3ccb9478
LornaVG <34290574+LornaVG@users.noreply.github.com>
2018-02-10 15:46:02 -0500

    Update Program.cs

--- src/TechJobsConsole/Program.cs
+++ src/TechJobsConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace TechJobsConsole
@@ -41,6 +41,7 @@ namespace TechJobsConsole
                     else
                     {
                         List<string> results = JobData.FindAll(columnChoice);
+                        results.Sort();
 
                         Console.WriteLine("\n*** All " + columnChoices[columnChoice] + " Values ***");
                         foreach (string item in results)
@@ -63,12 +64,32 @@ namespace TechJobsConsole
                     // Fetch results
                     if (columnChoice.Equals("all"))
                     {
-                        Console.WriteLine("Search all fields not yet implemented.");
+                        searchResults = JobData.FindByValue(searchTerm);
+
+                        if (searchResults.Count > 0)
+                        {
+                            PrintJobs(searchResults);
+                            //Console.WriteLine("Search all fields not yet implemented.");
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("No results found");
+                        }
                     }
                     else
                     {
                         searchResults = JobData.FindByColumnAndValue(columnChoice, searchTerm);
-                        PrintJobs(searchResults);
+
+                        if (searchResults.Count > 0)
+                        {
+                            PrintJobs(searchResults);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("No results found");
+                        }
                     }
                 }
             }
@@ -118,7 +139,19 @@ namespace TechJobsConsole
 
         private static void PrintJobs(List<Dictionary<string, string>> someJobs)
         {
-            Console.WriteLine("printJobs is not implemented yet");
+            foreach (Dictionary<string, string> job in someJobs)
+            {
+                Console.WriteLine("*****");
+
+                foreach (KeyValuePair<string, string> col in job)
+                {
+                    Console.WriteLine("{0}: {1}", col.Key, col.Value);
+                }
+
+                Console.WriteLine("*****\n");
+            }
+
+            //Console.WriteLine("printJobs is not implemented yet");
         }
     }
 }
